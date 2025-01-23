bca1c6889ecb9e86777defde15316dbfdc62de85
aamtayag <61495970+aamtayag@users.noreply.github.com>
2025-01-23 13:43:43 +1300

    Update Data-cleaning.py

--- week#6/Data-cleaning.py
+++ week#6/Data-cleaning.py
@@ -12,8 +12,8 @@ iris_df['class']=iris.target
 iris_df.columns=['sepal_len', 'sepal_wid', 'petal_len', 'petal_wid', 'class']
 #### ===> TASK 1: here - add two more lines of the code to find the number and mean of missing data
 cleaned_data = iris_df.dropna(how="all", inplace=True) # remove any empty lines
-x = iris_df.isnull().sum().sum()
-y = iris_df.isnull().mean().mean()
+x = iris_df.isnull().sum()
+y = iris_df.isnull().mean()
 
 iris_X=iris_df.iloc[:5,[0,1,2,3]]
 print(iris_X)
