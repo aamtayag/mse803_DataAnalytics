5a7b1921d793bb7d9e55b8cd0b19dcad326a30c0
aamtayag <61495970+aamtayag@users.noreply.github.com>
2025-01-23 13:39:14 +1300

    Update Data-cleaning.py

--- week#6/Data-cleaning.py
+++ week#6/Data-cleaning.py
@@ -12,9 +12,15 @@ iris_df['class']=iris.target
 iris_df.columns=['sepal_len', 'sepal_wid', 'petal_len', 'petal_wid', 'class']
 #### ===> TASK 1: here - add two more lines of the code to find the number and mean of missing data
 cleaned_data = iris_df.dropna(how="all", inplace=True) # remove any empty lines
-
+x = iris_df.isnull().sum().sum()
+y = iris_df.isnull().mean().mean()
 
 iris_X=iris_df.iloc[:5,[0,1,2,3]]
 print(iris_X)
 
+print(f"Number of missing values: {missing_count}")
+print(f"Mean of missing values: {missing_mean}")
+
 ### TASK2: Here - Write a short readme to explain above code and how we can calculate the corrolation amoung featuers with description
+
+
