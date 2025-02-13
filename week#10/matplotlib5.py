94afe9506e26a3584bc43224f2a4c6ade101a9e4
aamtayag <61495970+aamtayag@users.noreply.github.com>
2025-02-13 13:44:06 +1300

    update files

---
+++ week#10/matplotlib5.py
@@ -0,0 +1,17 @@
+
+# using matplotlib with data
+
+import matplotlib.pyplot as plt
+import pandas as pd
+data = pd.read_csv('/Users/aamtayag/Desktop/MSE/MSE803-DA/Week#10/Sample_Data_for_Activity.csv')
+x=[1,2,3,4,5]
+y=[10,20,25,30,40]
+plt.hist( data)
+
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Using Histogram')
+plt.savefig('fig5.png')
+plt.show()
+
+#plt.close()
