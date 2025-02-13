4705576a2c01bcd54b5590e4a7f7be55e8cb1d32
aamtayag <61495970+aamtayag@users.noreply.github.com>
2025-02-13 14:17:13 +1300

    Update matplotlib5.py

--- week#10/matplotlib5.py
+++ week#10/matplotlib5.py
@@ -4,18 +4,41 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 data = pd.read_csv('/Users/aamtayag/Desktop/MSE/MSE803-DA/Week#10/Sample_Data_for_Activity.csv')
+a = data['Normal_Distribution']
+b = data['Uniform_Distribution']
+c = data['Exponential_Distribution']
+d = data['Poisson_Distribution']
 #x=[1,2,3,4,5]
 #y=[10,20,25,30,40]
 #plt.hist( data['Exponential_Distribution'])
-plt.hist( data)
+#plt.hist(data)
+
+plt.subplot(2,2,1)
+plt.plot(a)
+plt.title('Normal_Distribution')
+#plt.savefig('fig5.png')
+
+plt.subplot(3, 2, 2)
+plt.plot(b)
+plt.title('Uniform_Distribution')
+
+
+plt.subplot(3, 2, 5)
+plt.plot(c)
+plt.title('Exponential_Distribution')
+
+
+plt.subplot(3, 2, 6)
+plt.plot(d)
+plt.title('Poisson_Distribution')
+
+
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
-plt.title('Using Histogram')
-plt.savefig('fig5.png')
-plt.show()
 
+
+
+plt.show()
 #plt.close()
 
 
-# I believe the best plot type to use here is a Histogram
-# You can see the value ranges, for each distribution type, from this chart type
