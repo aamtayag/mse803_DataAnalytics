83a135af44733279fc9328dacbe59d1ee0a28310
aamtayag <61495970+aamtayag@users.noreply.github.com>
2025-02-13 13:50:47 +1300

    Update matplotlib5.py

--- week#10/matplotlib5.py
+++ week#10/matplotlib5.py
@@ -6,8 +6,8 @@ import pandas as pd
 data = pd.read_csv('/Users/aamtayag/Desktop/MSE/MSE803-DA/Week#10/Sample_Data_for_Activity.csv')
 #x=[1,2,3,4,5]
 #y=[10,20,25,30,40]
+#plt.hist( data['Exponential_Distribution'])
 plt.hist( data)
-
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
 plt.title('Using Histogram')
@@ -15,3 +15,6 @@ plt.savefig('fig5.png')
 plt.show()
 
 #plt.close()
+
+
+# I believe the best plot type to use here is a Histogram
