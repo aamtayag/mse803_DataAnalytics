23af544f20076db9ed1255dc442c0837ad6c5d09
aamtayag <61495970+aamtayag@users.noreply.github.com>
2025-02-13 13:46:12 +1300

    Update matplotlib5.py

--- week#10/matplotlib5.py
+++ week#10/matplotlib5.py
@@ -4,8 +4,8 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 data = pd.read_csv('/Users/aamtayag/Desktop/MSE/MSE803-DA/Week#10/Sample_Data_for_Activity.csv')
-x=[1,2,3,4,5]
-y=[10,20,25,30,40]
+#x=[1,2,3,4,5]
+#y=[10,20,25,30,40]
 plt.hist( data)
 
 plt.xlabel('X-axis')
